276893df7fcb541714aa0058cf910af107caf769
Nick <nickshevr@gmail.com>
2026-03-14 19:59:55 +0300

    Lint TF

--- infra/terraform/envs/prod/main.tf
+++ infra/terraform/envs/prod/main.tf
@@ -32,17 +32,17 @@ resource "yandex_vpc_subnet" "main" {
 module "bot_vm" {
   source = "../../modules/bot_vm"
 
-  name_prefix           = var.name_prefix
-  zone                  = var.zone
-  subnet_id             = yandex_vpc_subnet.main.id
-  image_id              = var.image_id
-  repo_url              = var.repo_url
-  repo_ref              = var.repo_ref
-  telegram_bot_token    = var.telegram_bot_token
-  service_user          = var.service_user
-  app_dir               = var.app_dir
-  data_dir              = var.data_dir
-  cores                 = var.cores
-  memory_gb             = var.memory_gb
-  boot_disk_size_gb     = var.boot_disk_size_gb
+  name_prefix        = var.name_prefix
+  zone               = var.zone
+  subnet_id          = yandex_vpc_subnet.main.id
+  image_id           = var.image_id
+  repo_url           = var.repo_url
+  repo_ref           = var.repo_ref
+  telegram_bot_token = var.telegram_bot_token
+  service_user       = var.service_user
+  app_dir            = var.app_dir
+  data_dir           = var.data_dir
+  cores              = var.cores
+  memory_gb          = var.memory_gb
+  boot_disk_size_gb  = var.boot_disk_size_gb
 }
